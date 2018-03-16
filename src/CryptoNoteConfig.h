709daf68615f2568016d7724936af7143760287c
techqc <37153171+techqc@users.noreply.github.com>
2018-03-16 17:53:58 -0400

    a

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,10 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "sizd";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001fffffff
-ffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee4
-4ffbbd08807121014d7a8443937dbc3b0b9ca489a396763edad9bcf3109
-2586bd5fb194490d9bf5";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
